6341bb276de224f99e50a41b7639177214f599e7
DennisMelgar <165133370+DennisMelgar@users.noreply.github.com>
2024-03-27 01:21:15 -0600

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+   
+});
